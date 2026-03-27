af62304b17a11eb2d41424f9cb0e0cffa0f6eb9d
EM-H20 <ghd0106@gmail.com>
2026-03-27 15:27:41 +0900

    feat : Roulette.js 의 초기값 설정 #9

---
+++ src/Roulette.js
@@ -0,0 +1,40 @@
+import {
+  INITIAL_BALANCE,
+  NUMBER_OF_SLOTS,
+  COLOR_REWARD_RATE,
+  COLOR_SLOT_RANGES,
+} from "./constants.js";
+
+export default class Roulette {
+  constructor() {
+    this.balance = INITIAL_BALANCE;
+    this.round = 0;
+  }
+
+  calculatePayout(betAmount, result) {}
+
+  rouletteSpin() {}
+
+  //라운드 증가 메서드
+  addRound() {}
+
+  randomResult() {
+    return Math.floor(Math.random() * NUMBER_OF_SLOTS);
+  }
+
+  validateBet(color, betAmountValue) {
+    if (!color) {
+      throw new Error("색상을 선택해주세요.");
+    }
+    if (!/^[1-9]\d*$/.test(betAmountValue)) {
+      throw new Error("베팅 금액은 양의 정수여야 합니다.");
+    }
+    if (Number(betAmountValue) > this.balance) {
+      throw new Error("보유 금액을 초과할 수 없습니다.");
+    }
+  }
+
+  isWinner(color, result) {
+    return color === result;
+  }
+}
