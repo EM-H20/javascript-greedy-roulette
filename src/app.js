fbed7882fc8d3609ec8f896763df9f9bbbbe58d8
EM-H20 <ghd0106@gmail.com>
2026-03-27 16:14:58 +0900

    feat : app.js 초기구조 #9

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
 // 이벤트 리스너 + 흐름제어
+import Roulette from "./Roulette.js";
+import { INITIAL_BALANCE } from "./constants.js";
+import {
+  colorSelect,
+  betAmount,
+  betButton,
+  stopButton,
+  restartButton,
+  initRoulette,
+  updateMoneyDisplay,
+  updateRoundDisplay,
+  disableButtons,
+  enableButtons,
+  showSpinning,
+  showResult,
+  showBankruptMessage,
+  showEndGame,
+} from "./dom.js";
+
+betButton.addEventListener("click", () => {});
+
+stopButton.addEventListener("click", () => {});
+
+restartButton.addEventListener("click", () => {});
+
+const roulette = new Roulette();
+
+initRoulette(INITIAL_BALANCE);
+
+// 베팅 결과
+
+// 베팅 클릭
